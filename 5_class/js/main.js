2f0cf54ce0dde071c404cda97bfaf26241757974
Steven_Covilla <duvancovilla292@gmail.com>
2026-06-19 17:05:43 -0500

    5.3 con errore

--- 5_class/js/main.js
+++ 5_class/js/main.js
@@ -28,3 +28,49 @@ miPromesa.then((resultado) => {
 }).finally(() => {
     console.log("Proceso finalizado");
 });
+
+const miPromesa3 = new Promise((resolve, reject) => {
+    console.log("Creando Mi Promesa 3");
+
+    fetch("../data/equipos_mundial_2026.json").then(r => r.json()).then(data => {
+        console.log(typeof data);
+        console.log(data);
+        console.table(data.grupos);
+    });
+
+    const exito = true;
+    if(exito){
+        resolve("Operación Exitosa 3");
+    }else{
+        reject("Operacion Fallida 3");
+    }
+});
+
+console.log(typeof miPromesa);
+console.log(miPromesa);
+
+miPromesa3.then((resultado) => {
+    console.log("Exito: ", resultado);
+}).catch((error) => {
+    console.log("Error: ", error);
+}).finally(() => {
+    console.log("Proceso finalizado");
+});
+
+Promise.all([miPromesa, miPromesa2]).then((resultado) => {
+    console.log("Exito: ", resultado);
+}).catch((error) => {
+    console.log("Error: ", error);
+}).finally(() => {
+    console.log("Proceso finalizado");
+});
+
+async function cargarEquiposMundial(){
+    const respuesta = await fetch("../data/equipos_mundial_2026.json");
+    const datos = await respuesta.json();
+    console.log(typeof datos);
+    console.log(datos);
+    console.table(datos.equipos);
+};
+ 
+cargarEquiposMundial();
